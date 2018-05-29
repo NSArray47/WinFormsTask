7229e7f7ecfd055a13ac319fefda8e3f4b863477
NSArray47 <2ndriy@gmail.com>
2018-05-29 13:11:14 +0300

    paiting fixes
    добавлено: якщо протягувати лінію, то її видно впродовж протягування

--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -13,22 +13,28 @@ namespace WindowsFormsApp2
     public partial class Form1 : Form
     {
         private Graphics graphics;
+        private Graphics trGraphics;
         private int x1 = -1;
         private int y1 = -1;
         private int y2 = -1;
         private int x2 = -1;
         private Pen pen;
+        private Pen pen2;
         private bool isSaved = false;
+        private bool startedDrawing = false;
         private List<Line> lines = new List<Line>();
 
         public Form1()
         {
-            
             InitializeComponent();
+            //trGraphics = panel2.CreateGraphics();
+            //trGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
             graphics = panel1.CreateGraphics();
-            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
             pen = new Pen(Color.Black, 4);
             pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen2 = new Pen(Color.Black, 4);
+            pen2.StartCap = pen2.EndCap = System.Drawing.Drawing2D.LineCap.Round;
         }
 
         private void whiteToolStripMenuItem_Click(object sender, EventArgs e)
@@ -43,24 +49,56 @@ namespace WindowsFormsApp2
 
         private void onMouseDown(object sender, MouseEventArgs e)
         {
-            x1 = e.X;
-            y1 = e.Y;
+            x1 = x2 = e.X;
+            y1 = y2 = e.Y;
             panel1.Cursor = Cursors.Cross;
+            panel1.Invalidate();
+            startedDrawing = true;
         }
 
         private void onMouseUp(object sender, MouseEventArgs e)
         {
             x2 = e.X;
             y2 = e.Y;
+            panel1.Refresh();
+            foreach (var item in lines)
+            {
+                pen2.Color = ColorTranslator.FromHtml(item.HtmlColor);
+                graphics.DrawLine(pen2, new Point(item.X1, item.Y1), new Point(item.X2, item.Y2));
+            }
             graphics.DrawLine(pen, new Point(x1, y1), new Point(x2, y2));
             lines.Add(new Line(x1, y1, x2, y2, ColorTranslator.ToHtml(pen.Color)));
             panel1.Cursor = Cursors.Default;
             isSaved &= false;
+            x2 = -1;
+            y2 = -1;
+            startedDrawing = false;
         }
+
+        private int dist(int x1, int y1, int x2, int y2)
+        {
+            return (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+        }
+
 
         private void onMouseMove(object sender, MouseEventArgs e)
         {
-            //TODO: nice drawings      
+            if (startedDrawing == true)
+            {
+                if (dist(e.X, e.Y, x2, y2) >= 16)
+                {
+                    x2 = e.X;
+                    y2 = e.Y;
+                    panel1.Refresh();
+                    foreach (var item in lines)
+                    {
+                        pen2.Color = ColorTranslator.FromHtml(item.HtmlColor);
+                        graphics.DrawLine(pen2, new Point(item.X1, item.Y1), new Point(item.X2, item.Y2));
+                    }
+                    graphics.DrawLine(pen, new Point(x1, y1), new Point(e.X, e.Y));
+                }
+            }
+            
         }
 
         private void colorToolStripMenuItem_Click(object sender, EventArgs e)
